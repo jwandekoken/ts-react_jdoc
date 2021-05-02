3fe60877be49804e4d5db1ad8bb2669339f3d876
jwandekoken <julio.wandekoken@paketa.com.br>
2021-05-02 12:07:19 -0300

    breaking up resolve logic with filters

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -10,22 +10,21 @@ export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
+      // if args.path equal 'index.js' - handle root index.js file
+      build.onResolve({ filter: /(^index\.js$)/ }, () => ({
+        path: "index.js",
+        namespace: "a",
+      }));
+
+      // if args.path have "./" or "../" on it - handle relative paths in a module
+      build.onResolve({ filter: /^\.+\// }, (args: any) => ({
+        namespace: "a",
+        path: new URL(args.path, "https://unpkg.com" + args.resolveDir + "/")
+          .href,
+      }));
+
+      // if args.path have ".anything" - handle main file of a module
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log("onResolve", args);
-        if (args.path === "index.js") {
-          return { path: args.path, namespace: "a" };
-        }
-
-        if (args.path.includes("./") || args.path.includes("../")) {
-          return {
-            namespace: "a",
-            path: new URL(
-              args.path,
-              "https://unpkg.com" + args.resolveDir + "/"
-            ).href,
-          };
-        }
-
         return {
           namespace: "a",
           path: `https://unpkg.com/${args.path}`,
